694c707baa90349bcd5c526f187250ffb3dc3aed
avatarr <prachaya.su@gmail.com>
2012-06-15 14:00:35 +0700

    new code
    new code

--- Image2D/src/image2d/Image2D.java
+++ Image2D/src/image2d/Image2D.java
@@ -14,6 +14,6 @@ public class Image2D {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello ,World!");
     }
 }
